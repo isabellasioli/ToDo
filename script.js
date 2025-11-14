a601bb34527af453c6eace2e3874a93e3a2c0629
Isabella <isabellasioli86@gmail.com>
2025-11-14 17:29:28 -0300

    Modificações

--- script.js
+++ script.js
@@ -30,26 +30,43 @@ function iniciaToDO() {
     btn_nova_tarefa.addEventListener("click", adicionarTarefa);
     // Associa a função "adicionarTarefaEnter()" ao evento de clicar atecla "Enter" no campo de "Adicionar nova tarefa..."
     txt_nova_tarefa.addEventListener("keypress", adicionarTarefaEnter);
+    
+    // Carrega as tarefas salvas no cookie do navegador Web ao carregar a página 
+    const arrayTarefas = ObterTarefasDoNavegador();
+    
+    salvarCookieTarefas([]);
+    arrayTarefas.forEach(strTarefa => {
+        adicionarTarefa(strTarefa);
+    });
 }
 
-function adicionarTarefa() {
+function adicionarTarefa(strTarefa) {
+
+    if (typeof strTarefa !== 'string' || strTarefa == null) {
+        // Atribui como valor da variável 'strTarefas' o texto digitado na caixa de texto 'Adicionar nova tarefa
+        strTarefa = txt_nova_tarefa.value; 
+    }
+
     // Se a caixa de texto não está vazia .trim() remove espaços em branco do começo e fim do valor do campo.
-    if (txt_nova_tarefa.value.trim() !== "") {
+    if (strTarefa.trim() !== "") {
         const btn_item = ` 
         <div>
             <button class="btn btn-sm me-2 btn-concluir" onclick="concluirTarefa(this)">Concluir</button>
             <button class="btn btn-sm btn-excluir" onclick="obterIDTarefaExcluir(this);modalExcluir.show()">Excluir</button>
         </div>
         `;
-
+        
         // Cria um novo item a lista.
         const item = document.createElement("li");
         item.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
         // Adiciona o texto digitado na caixa de texto e os botões para concluir e excluir a tarefa.
         // 75% vamos usar para a escrita, já os outros 25% serão usados para os botões.
         // "text-truncate" corta e adiciona reticências (três pontinhos ...) em nomes de tarefas que exedem os 75% do tamanho da linha.
-        item.innerHTML = "<span class='w-75 text-truncate'>" + txt_nova_tarefa.value + "</span>" + btn_item;
-
+        item.innerHTML = "<span class='w-75 text-truncate'>" + strTarefa + "</span>" + btn_item;
+        
+        // Adiciona o item aos cookies do navegador 
+        adicionarTarefaAoCookie(strTarefa);
+        
         // Adiciona o item a lista de tarefas.
         lista_tarefas.appendChild(item);
     }
@@ -68,21 +85,26 @@ function adicionarTarefaEnter(evento) {
 function concluirTarefa(btn_concluir) {
     // Reproduz o áudio ao clicar no botão "Concluir".
     audioConcluir.play();
-
+    
     // Joga os confettis na tela.
     for (let i = 0; i <= 50; i++) {
         confetti();
     }
-
+    
     // Atualiza o ID da tarefa a ser exluída e passa como parâmetro o botão de "Concluir" clickado.
     obterIDTarefaExcluir(btn_concluir);
-
+    
     // Chama a função JS "excluirTarefa()".
     excluirTarefa();
-
+    
 }
 
 function excluirTarefa() {
+    // Remove a tarefa do cookie do navegador
+    const arrayTarefas = ObterTarefasDoNavegador(); // Carrega as tarefas para um vetor à partir do cookie do navegador 
+    arrayTarefas.splice(id_tarefa_excluir, 1); // Remove 1 tarefa do vetor à partir do ID da tarefa excluída 
+    salvarCookieTarefas(arrayTarefas); // Atualiza o cookie do navegador após excluir a tarefa
+
     // Remove o item da lista de tarefa.
     lista_tarefas.removeChild(lista_tarefas.children[id_tarefa_excluir]);
     // Fecha o modal de "Excluir tarefa".
@@ -98,8 +120,46 @@ function obterIDTarefaExcluir(btn) {
     id_tarefa_excluir = tarefas.indexOf(item);
 }
 
+// --------------------------------------------------------------------------
+// 3. COOKIE
+// Adiciona funcionalidade de cookies (persistência) das tarefas adicionadas
+// (matém as tarefas adiconadas ao fechar ou atualizar a página)
+// --------------------------------------------------------------------------
+const CART_STORAGE_TODO = 'tarefas_todo';
+
+function ObterTarefasDoNavegador() {
+    // Tenta ler o cookie do navegador.
+    try {
+        const cookie = localStorage.getItem(CART_STORAGE_TODO);
+        if (cookie) {
+            return JSON.parse(cookie);
+        }
+    } catch (e) {
+        console.error("Falha ao ler o cookie do armazenamento local.");
+    }
+    // Retorna um vetor vazio em caso de falha.
+    return [];
+}
+
+function salvarCookieTarefas(arrayTarefas) {
+    try {
+        // Salva as tarefas em formato JSON no navegador Web.
+        // Você pode visualizar os itens salvos no navegador Web em:
+        // Botão direito na página > Inspecionar > Application > Stroge > Local stroge.
+        localStorage.setItem(CART_STORAGE_TODO, JSON.stringify(arrayTarefas));
+    } catch (e) {
+        console.error("ERRO: Falha ao salvar carrinho no navegador. Erro: ", e);
+    }
+}
+
+function adicionarTarefaAoCookie(strTarefa) {
+    const arrayTarefas = ObterTarefasDoNavegador(); // Obtém as tarefas atuais do cookie do navegador Web em formato de vetor.
+    arrayTarefas.push(strTarefa); // Adiciona a tarefa recebida como parâmetro da função ao cookie do navegador Web.
+    salvarCookieTarefas(arrayTarefas); // Salva o cookie com a tarefa adicionada no navegador Web.
+}
+
 // -------------------------------------------------------
-// 3. ESCUTADORES DE EVENTOS E INÍCIO 
+// 4. ESCUTADORES DE EVENTOS E INÍCIO 
 // -------------------------------------------------------
 
 iniciaToDO();
